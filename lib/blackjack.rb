c0b985bb224ca590448497c1a1156ccd40dea1ba
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-11 14:56:23 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -43,7 +43,7 @@ def hit?(number)
 end
 
 def invalid_command
-    print "Please enter a valid command"
+   expect($stdout).to receive(:puts).with("Please enter a valid command")
 end
 
 #####################################################
